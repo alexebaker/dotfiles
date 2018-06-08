c8a849bc1f14eb9d0b669bf5269b4af5dfbd0559
Alexander Baker <alexebaker@alexebaker.com>
2018-06-08 14:40:28 -0600

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
+
 git clone git@github.com:alexebaker/dotfiles.git ~/.dotfiles
+
+
+cd ~/
+rm .vimrc
+ln -s ~/.dotfiles/vim/vimrc .vimrc
+rm .zshrc
+ln -s ~/.dotfiles/zsh/zshrc .zshrc
+rm -r .zsh_custom
+ln -s ~/.dotfiles/zsh/zsh_custom .zsh_custom
+
