c7999cc509a72fadb480a2810ab5c5ae2ed9a9ee
Alexander Baker <aebaker@sandia.gov>
2018-06-25 16:25:38 -0600

    updated install script and fixed vimrc

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 
 git clone https://github.com/alexebaker/dotfiles.git ~/.dotfiles
-
+git clone --recursive https://github.com/joel-porquet/zsh-dircolors-solarized.git $ZSH_CUSTOM/plugins/zsh-dircolors-solarized
 
 cd ~/
 rm .vimrc
@@ -12,3 +12,7 @@ ln -s ~/.dotfiles/zsh/zshrc .zshrc
 rm -r .zsh_custom
 ln -s ~/.dotfiles/zsh/zsh_custom .zsh_custom
 
+brew tap oclint/formulae
+brew install flake8 cppcheck oclint yamllint jsonlint
+gem install mdl puppet-lint
+pip install rstcheck
