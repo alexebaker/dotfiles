39b0ad1a035d3e3b82c780e353041da991dc3c96
Alexander Baker <alexebaker@alexebaker.com>
2019-03-08 22:30:42 -0700

    fixed brew for linux stuff

--- zsh/zsh_custom/02-custom.zsh
+++ zsh/zsh_custom/02-custom.zsh
@@ -9,11 +9,6 @@ set editing-mode vi
 
 export HOMEBREW_NO_ANALYTICS=1
 
-# Add brew directories to path
-addToPath /usr/local/sbin before
-addToPath /usr/local/bin before
-addToPath /usr/local/opt/tcl-tk/bin before
-
 set editing-mode vi
 
 export IP_ADDR=`ifconfig -a | grep inet | grep -v '127.0.0.1' | awk '{print $2}' | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`
