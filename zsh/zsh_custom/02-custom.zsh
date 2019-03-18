9a4d1144c772fbad8363b726df18f6d77055a849
Alexander Baker <alexebaker@alexebaker.com>
2019-03-18 16:27:41 -0600

    remived ip addr env

--- zsh/zsh_custom/02-custom.zsh
+++ zsh/zsh_custom/02-custom.zsh
@@ -15,7 +15,3 @@ fi
 set editing-mode vi
 
 export HOMEBREW_NO_ANALYTICS=1
-
-if [[ ! -f /etc/os-release ]] ; then
-    export IP_ADDR=`ifconfig -a | grep inet | grep -v '127.0.0.1' | awk '{print $2}' | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`
-fi
